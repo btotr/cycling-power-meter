445e711e067166dfe486cbcace634df643e8ea59
Colin Meerveld <colin.meerveld@posteo.me>
2024-05-13 18:50:20 +0200

    refactor class dependencies

--- boot.py
+++ boot.py
@@ -38,61 +38,50 @@ class BLE_Cycling_Power:
         self.battery_level_characteristic = aioble.Characteristic(self.battery_service, bluetooth.UUID(0x2A19), read=True, notify=True)
         # register services
         aioble.register_services(self.power_service, self.device_information_service, self.battery_service)
+        # Connections
+        self.connections = []
+        self.last_published_time = 0
 
-    def publish_task(self, connection):
-        power = 0
-        last_time = 0
-        diff_time = 0
-        while True:
-            '''
-            TODO power calculation and update and frequency is based on the revolutions.
-            Need to use something like a MPU-6050 accelerometer
-            '''
-            revolutions = cadance.get_revolutions()-cadance.get_lastRevolutions()
-            if revolutions == 0:
-                return
+    def publish_task(self, revolutions, lastRevTime, force, battery_level):
+        # power calculation
+        mp = 2 # rouvy=2 garmin=1 TODO sniff brand
+        newton_ratio = 0.00980665 # 1 gram
+        meter_per_revolution = 1.09956 #2PI*0.175 need to use bluetooth opcode to set the crunk size
+        now = time.time_ns()
+        diff_time = (now - self.last_published_time)/1e9
+        power = int(abs(force*newton_ratio*mp)*(meter_per_revolution/diff_time))
+        
 
-            # power calculation
-            mp = 2 # rouvy=2 garmin=1 TODO sniff brand
-            newton_ratio = 0.00980665 # 1 gram
-            meter_per_revolution = 1.09956 #2PI*0.175 need to use bluetooth opcode to set the crunk size
-            now = time.time_ns()
-            diff_time = (now - last_time)/1e9
-            power = int(abs(weight.get_weight()*newton_ratio*mp)*(revolutions*meter_per_revolution)/diff_time)
-            
+        # debugging 
+        rpm = 60 / diff_time
+        print("force: {:0.2f}".format(force))
+        #print("last rev: {}".format(cadance.get_lastRevTime()))
+        print("power: {}".format(power))
+        #print("revolutions: {}".format(cadance.get_revolutions()))
+        #print("rpm: {}".format(rpm))
+        #print("time: {}".format(diff_time))
 
-            # debugging 
-            rpm = 60 * revolutions / diff_time
-            #print("force: {:0.2f}".format(weight.get_weight()))
-            #print("last rev: {}".format(cadance.get_lastRevTime()))
-            print("{}".format(power))
-            # print("revolutions: {}".format(cadance.get_revolutions()))
-            # print("rpm: {}".format(rpm))
-            #print("time: {}".format(diff_time))
-
-            # bluetooth packets
-            battery_level =  struct.pack('<B', int(battery.get_level()))
-            power_data =  struct.pack('<8B',
-                0x20, 
-                0x00, 
-                power & 0xff, 
-                power >> 8, 
-                cadance.get_revolutions() & 0xff, 
-                cadance.get_revolutions() >> 8,
-                cadance.get_lastRevTime() & 0xff, 
-                cadance.get_lastRevTime() >> 8)
-            # publish packets
-            self.battery_level_characteristic.notify(connection, battery_level)
-            self.battery_level_characteristic.write(battery_level)
-            self.measurement_characteristic.notify(connection, power_data)
+        # bluetooth packets
+        battery_level_data =  struct.pack('<B', int(battery_level))
+        power_data =  struct.pack('<8B',
+            0x20, 
+            0x00, 
+            power & 0xff, 
+            power >> 8, 
+            revolutions & 0xff, 
+            revolutions >> 8,
+            lastRevTime & 0xff, 
+            lastRevTime >> 8)
+        # publish packets
+        for c in self.connections:
+            self.battery_level_characteristic.notify(c, battery_level_data)
+            self.battery_level_characteristic.write(battery_level_data)
+            self.measurement_characteristic.notify(c, power_data)
             self.measurement_characteristic.write(power_data)
-            
-            # reset weight, time and cadance
-            weight.set_weight(0)
-            cadance.set_lastRevolutions(cadance.get_revolutions())
-            last_time = now
-            
-            await asyncio.sleep_ms(1000)
+        
+        # save time 
+        self.last_published_time = now
+        
 
     async def server_task(self, connection):
         # write power specific information
@@ -102,17 +91,19 @@ class BLE_Cycling_Power:
         self.manufacturer_characteristic.write(struct.pack('<12s', b'open-kinetic'))
         self.software_rev_characteristic.write(struct.pack('<6s', b'v0.1.1'))
         print("Connection from", connection.device)
+        self.connections.append(connection)
         while connection.is_connected():
-            await cycling_power.publish_task(connection)
+            # TODO handeling opcode
+            await asyncio.sleep(60)
 
     async def connection_task(self):
         while True:
-            connection =  await aioble.advertise (
+           connection =  await aioble.advertise (
                 250_000,
                 name="open-kinetic-pwr",
                 services=[self.power_service_uuid, self.device_information_service_uuid],
                 appearance=1156)
-            asyncio.create_task(self.server_task(connection))
+           asyncio.create_task(self.server_task(connection))
 
 '''
 
@@ -122,7 +113,7 @@ battery
 
 class Battery:
 
-    def __init__(self, pin_adc=5):
+    def __init__(self, pin_adc):
         self.level = 60
         self.adc = ADC(Pin(pin_adc)) 
         self.adc.atten(ADC.ATTN_11DB)
@@ -144,7 +135,7 @@ weight
 
 class Weight:
 
-    def __init__(self, pin_out=2, pin_clk=3, cf=35):
+    def __init__(self, pin_out, pin_clk, cf=35):
         self.taste=Pin(1,Pin.IN,Pin.PULL_UP)
         self.weight = 0
         self.hx = HX711(Pin(pin_out),Pin(pin_clk),1)
@@ -173,22 +164,19 @@ cadance
 
 class Cadance:
 
-    def __init__(self, pin_hall=7):
+    def __init__(self, pin_hall):
         self.revolutions = 0
         self.lastRevTime = 0
         self.lastRevolutions = 0
         self.hall_sensor = Pin(pin_hall,Pin.IN)
         self.hall_sensor.irq(trigger=Pin.IRQ_FALLING, handler=self.hall_sensor_task)
-        self.last_time = time.time()
+        self.callback = None
 
     def get_revolutions(self):
         return self.revolutions
     
-    def set_lastRevolutions(self, revolutions):
-        self.lastRevolutions = revolutions
-    
-    def get_lastRevolutions(self):
-        return self.lastRevolutions
+    def set_callback(self, callback):
+        self.callback = callback
 
     def get_lastRevTime(self):
         return self.lastRevTime
@@ -198,18 +186,29 @@ class Cadance:
         now = time.ticks_ms()
         now_1024 = now % 65536 # rollover 64000 sec / 1000 * 1024
         self.lastRevTime =  now_1024
+        self.callback()
 
 # Run tasks
-async def main():
+async def tasks():
     t1 = asyncio.create_task(battery.level_task())
     t2 = asyncio.create_task(cycling_power.connection_task())
     t4 = asyncio.create_task(weight.load_sensor_task())
     await asyncio.gather(t1, t2, t4)
 
-cadance = Cadance()
-weight = Weight(2,3,225)
-battery = Battery()
+# main 
 cycling_power = BLE_Cycling_Power()
-asyncio.run(main())
+cadance = Cadance(7)
+weight = Weight(2, 3, 225)
+battery = Battery(5)
+
+def handle_revolution_update():
+    cycling_power.publish_task(cadance.get_revolutions(),
+                               cadance.get_lastRevTime(), 
+                               weight.get_weight(), 
+                               battery.get_level())
+    weight.set_weight(0)
+
+cadance.set_callback(handle_revolution_update)
+asyncio.run(tasks())
 
 
